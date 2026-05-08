c73530d7f70c1b9847657d989844c8f9fd713924
KouraTraore <traorekourasouleymane@gmail.com>
2026-05-08 15:17:29 +0000

    modification des routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/admin/dashboard', [dashdordController::class, 'index'])->name('admin.index');
-Route::get('/admin/category', [CategoriesController::class, 'index'])->name('admin.category');
+Route::get('/admin/category', [CategoryController::class, 'index'])->name('admin.category');
 Route::get('/admin/produits', [ProduitsController::class, 'index'])->name('admin.produits');
 route::get('/admin/factures', [ReportController::class, 'index'])->name('admin.factures');
 route::get('/admin/docs', [DocsController::class, 'index'])->name('admin.docs');
