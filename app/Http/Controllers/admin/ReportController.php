aa17f1dc6fe64c2ac39d360139f71f0f98f09683
KouraTraore <traorekourasouleymane@gmail.com>
2026-05-09 08:26:58 +0000

    modification de la page admin et routes

--- app/Http/Controllers/admin/ReportController.php
+++ app/Http/Controllers/admin/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Facture;
+use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
@@ -12,5 +13,52 @@ class ReportController extends Controller
         $factures = Facture::all();
 
         return view('admin.factures', compact('factures'));
+    }
+
+    public function create()
+    {
+        return view('admin.add_facture');
+    }
+
+    public function store(Request $request)
+    {
+        Facture::create([
+            'client_id' => $request->client_id,
+            'commande_id' => $request->commande_id,
+            'montant_total' => $request->montant_total,
+            'statut' => $request->statut
+        ]);
+
+        return redirect()->route('admin.factures');
+    }
+
+    public function edit($id)
+    {
+        $facture = Facture::findOrFail($id);
+
+        return view('admin.edit_facture', compact('facture'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $facture = Facture::findOrFail($id);
+
+        $facture->update([
+            'client_id' => $request->client_id,
+            'commande_id' => $request->commande_id,
+            'montant_total' => $request->montant_total,
+            'statut' => $request->statut
+        ]);
+
+        return redirect()->route('admin.factures');
+    }
+
+    public function delete($id)
+    {
+        $facture = Facture::findOrFail($id);
+
+        $facture->delete();
+
+        return redirect()->route('admin.factures');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,12 @@ Route::get('/admin/produits', [ProductController::class, 'index'])->name('admin.
 route::get('/admin/factures', [ReportController::class, 'index'])->name('admin.factures');
 route::get('/admin/docs', [DocsController::class, 'index'])->name('admin.docs');
 route::get('/admin/commandes', [ErrorsController::class, 'index'])->name('admin.commandes');
+Route::get('/admin/factures/create', [ReportController::class, 'create'])->name('admin.factures.create');
+
+Route::post('/admin/factures/store', [ReportController::class, 'store'])->name('admin.factures.store');
+
+Route::get('/admin/factures/edit/{id}', [ReportController::class, 'edit'])->name('admin.factures.edit');
+
+Route::post('/admin/factures/update/{id}', [ReportController::class, 'update'])->name('admin.factures.update');
+
+Route::get('/admin/factures/delete/{id}', [ReportController::class, 'delete'])->name('admin.factures.delete');
