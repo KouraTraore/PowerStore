879cd4cad23d3235a271e584b55a1b19642f4767
KouraTraore <traorekourasouleymane@gmail.com>
2026-05-15 13:05:05 +0000

    conflit corrige

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-<<<<<<< HEAD
+
 
     public $timestamps = false;
 
@@ -104,7 +104,7 @@ class Category extends Model
         }
         return $query;
     }
-=======
+
     //
->>>>>>> origin/mousstafa_features
+
 }
